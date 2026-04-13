5a76690cf023915d61b37abf3004684ef2c9a315
maheshgajjela221 <166375267+maheshgajjela221@users.noreply.github.com>
2026-04-13 11:44:04 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then(res => res.json())
+      .then(data => setMsg(data.message));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Docker App 🚀</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
